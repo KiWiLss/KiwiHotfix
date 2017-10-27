28307e08339ac8b8d0f5c3cf61e72dde4053605c
KiwiLss <m15205622327@163.com>
2017-10-27 10:08:04 +0800

    10-27

--- scrolling/src/main/java/com/winding/scrolling/activity/ComplexCoordinatorActivity.java
+++ scrolling/src/main/java/com/winding/scrolling/activity/ComplexCoordinatorActivity.java
@@ -5,14 +5,13 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.AppBarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.winding.scrolling.R;
-
-import static android.R.attr.offset;
 
 /**
  * Created by 刘少帅 on 2017/10/26
@@ -24,6 +23,7 @@ public class ComplexCoordinatorActivity extends AppCompatActivity {
     private static final float PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR  = 0.9f;
     private static final float PERCENTAGE_TO_HIDE_TITLE_DETAILS     = 0.3f;
     private static final int ALPHA_ANIMATIONS_DURATION              = 200;
+    public static final String TAG = "MMM";
 
     private boolean mIsTheTitleVisible          = false;
     private boolean mIsTheTitleContainerVisible = true;
@@ -46,24 +46,28 @@ public class ComplexCoordinatorActivity extends AppCompatActivity {
             public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
 
                 int maxScroll = appBarLayout.getTotalScrollRange();
-                float percentage = (float) Math.abs(offset) / (float) maxScroll;
+                float percentage = (float) Math.abs(verticalOffset) / (float) maxScroll;
 
+                Log.e(TAG, "onOffsetChanged: "+percentage );
                 handleAlphaOnTitle(percentage);
                 handleToolbarTitleVisibility(percentage);
 
             }
         });
+        //mTitle.setVisibility(View.GONE);
 
         //mToolbar.inflateMenu(R.menu.menu_main);
         startAlphaAnimation(mTitle, 0, View.INVISIBLE);
+//        mIsTheTitleVisible=mTitle.getVisibility()==View.VISIBLE?true:false;
     }
 
 
     private void handleToolbarTitleVisibility(float percentage) {
-        if (percentage >= PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR) {
+        if (percentage >= PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR) {//大于0.3
 
             if(!mIsTheTitleVisible) {
                 startAlphaAnimation(mTitle, ALPHA_ANIMATIONS_DURATION, View.VISIBLE);
+                //mTitle.setVisibility(View.VISIBLE);
                 mIsTheTitleVisible = true;
             }
 
@@ -71,6 +75,7 @@ public class ComplexCoordinatorActivity extends AppCompatActivity {
 
             if (mIsTheTitleVisible) {
                 startAlphaAnimation(mTitle, ALPHA_ANIMATIONS_DURATION, View.INVISIBLE);
+                //mTitle.setVisibility(View.GONE);
                 mIsTheTitleVisible = false;
             }
         }
@@ -80,6 +85,7 @@ public class ComplexCoordinatorActivity extends AppCompatActivity {
         if (percentage >= PERCENTAGE_TO_HIDE_TITLE_DETAILS) {
             if(mIsTheTitleContainerVisible) {
                 startAlphaAnimation(mTitleContainer, ALPHA_ANIMATIONS_DURATION, View.INVISIBLE);
+                //mTitleContainer.setVisibility(View.GONE);
                 mIsTheTitleContainerVisible = false;
             }
 
@@ -87,6 +93,7 @@ public class ComplexCoordinatorActivity extends AppCompatActivity {
 
             if (!mIsTheTitleContainerVisible) {
                 startAlphaAnimation(mTitleContainer, ALPHA_ANIMATIONS_DURATION, View.VISIBLE);
+                //mTitleContainer.setVisibility(View.VISIBLE);
                 mIsTheTitleContainerVisible = true;
             }
         }
@@ -94,19 +101,20 @@ public class ComplexCoordinatorActivity extends AppCompatActivity {
 
 
     private void bindActivity() {
-        mToolbar        = (Toolbar) findViewById(R.id.main_toolbar);
-        mTitle          = (TextView) findViewById(R.id.main_textview_title);
+        mToolbar= (Toolbar) findViewById(R.id.main_toolbar);
+        mTitle= (TextView) findViewById(R.id.main_textview_title);
         mTitleContainer = (LinearLayout) findViewById(R.id.main_linearlayout_title);
         mAppBarLayout   = (AppBarLayout) findViewById(R.id.main_appbar);
     }
 
     public static void startAlphaAnimation (View v, long duration, int visibility) {
+
         AlphaAnimation alphaAnimation = (visibility == View.VISIBLE)
                 ? new AlphaAnimation(0f, 1f)
                 : new AlphaAnimation(1f, 0f);
-
         alphaAnimation.setDuration(duration);
         alphaAnimation.setFillAfter(true);
         v.startAnimation(alphaAnimation);
+
     }
 }
