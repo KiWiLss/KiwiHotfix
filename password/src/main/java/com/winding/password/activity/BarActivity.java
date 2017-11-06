eac2b95cbe92da43a97d9e35884c31537c9db46d
KiwiLss <m15205622327@163.com>
2017-11-06 16:17:26 +0800

    11-6

--- password/src/main/java/com/winding/password/activity/BarActivity.java
+++ password/src/main/java/com/winding/password/activity/BarActivity.java
@@ -5,12 +5,14 @@ import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.daimajia.numberprogressbar.NumberProgressBar;
 import com.daimajia.numberprogressbar.OnProgressBarListener;
 import com.winding.password.R;
 import com.winding.password.widget.RoundProgressBar;
+import com.winding.password.widget.WaveProgressView;
 
 /**
  * Created by 刘少帅 on 2017/11/6
@@ -29,6 +31,8 @@ public class BarActivity extends AppCompatActivity {
          npbOne = (NumberProgressBar) findViewById(R.id.npb_bar_one);
         NumberProgressBar npbTwo = (NumberProgressBar) findViewById(R.id.npb_bar_two);
         mRpb = (RoundProgressBar) findViewById(R.id.rpb_bar_bar);
+        final ProgressBar pbPb = (ProgressBar) findViewById(R.id.pb_bar_pb);
+
 
         npbOne.setOnProgressBarListener(new OnProgressBarListener() {
             @Override
@@ -39,12 +43,14 @@ public class BarActivity extends AppCompatActivity {
         npbTwo.setPrefix("csdn");
         npbOne.setProgress(0);
 
+        pbPb.setProgress(0);
         //定时任务
         mCrt = new CountDownTimer(50*5, 50) {//执行4次
             @Override
             public void onTick(long l) {
                 mPb+=10;
                 npbOne.setProgress(mPb);
+                pbPb.setProgress(mPb);
             }
 
             @Override
@@ -55,6 +61,21 @@ public class BarActivity extends AppCompatActivity {
 
         mRpb.setMax(100);
         mRpb.setTextSize(40f);
+
+
+        WaveProgressView wpv = (WaveProgressView) findViewById(R.id.wpv);
+        wpv.setProgressNum(60,600);
+        wpv.setOnAnimationListener(new WaveProgressView.OnAnimationListener() {
+            @Override
+            public String howToChangeText(float interpolatedTime, float updateNum, float maxNum) {
+                return null;
+            }
+
+            @Override
+            public float howToChangeWaveHeight(float percent, float waveHeight) {
+                return 0;
+            }
+        });
     }
     int mPb;
     public void addListener(View view) {
